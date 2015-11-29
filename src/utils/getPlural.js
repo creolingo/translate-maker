b51eca0ed884a7fff014dd6e9c252f6fba47fcc9
Zlatko Fedor <zfedor@gmail.com>
2015-11-29 12:25:51 +0100

    added getPlural

---
+++ src/utils/getPlural.js
@@ -0,0 +1,26 @@
+import plurals from 'make-plural/plurals';
+import keys from 'lodash/object/keys';
+import localeResolver from 'locale';
+
+const supported = new localeResolver.Locales(keys(plurals));
+
+let cachePlural = null;
+
+function localeToLanguage(locale, defaultValue) {
+  const language = (new localeResolver.Locales(locale)).best(supported).toString();
+
+  return language || defaultValue;
+}
+
+export default function getPlural(locale, defaultLanguage = 'en') {
+  if (!cachePlural || cachePlural.locale !== locale) {
+    const language = localeToLanguage(locale, defaultLanguage);
+
+    cachePlural = {
+      locale,
+      fn: plurals[language],
+    };
+  }
+
+  return cachePlural.fn;
+}
