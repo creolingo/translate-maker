7c361ee38f7b89d6baaa2b9389e906bd08996f66
Zlatko Fedor <zfedor@gmail.com>
2016-01-06 12:04:14 +0100

    added xhr adapter

---
+++ src/adapters/XHR.js
@@ -0,0 +1,64 @@
+import Adapter from './Adapter';
+
+function request(url, parse, callback) {
+  try {
+    const xhr = new (XMLHttpRequest || ActiveXObject)('MSXML2.XMLHTTP.3.0');
+    xhr.open('GET', url, 1);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState > 3) {
+        callback(null, parse(x.responseText));
+    };
+    xhr.send(data);
+  } catch (e) {
+    callback(e);
+  }
+}
+
+function getPath(options, namespace, fileName) {
+  if (!namespace) {
+    return `${options.path}/${fileName}`;
+  }
+
+  const mainPath = options.namespacePath || options.path;
+  const directory = namespace.replace('.', '/'); // replace com.data => com/data
+
+  return `${mainPath}/${directory}/${fileName}`;
+}
+
+const defaultOptions = {
+  path: void 0,
+  getPath,
+  parse: JSON.parse,
+  ext: '.json',
+};
+
+export default class XHR extends Adapter {
+  constructor(options = {}) {
+    if (!options.path) {
+      throw new Error('You need to set path');
+    }
+
+    super({
+      ...defaultOptions,
+      ...options,
+    });
+  }
+
+  get(locale, namespace, callback) {
+    if (typeof namespace === 'function') {
+      return this.get(locale, null, namespace);
+    }
+
+    const options = this.getOptions();
+    const fileName = `${locale}${options.ext || ''}`;
+    const path = options.getPath(options, namespace, fileName);
+
+    request(path, options.parse, callback);
+  }
+
+  set(locale, value, namespace, callback) {
+    throw new Error('XHR adapter is read only');
+  }
+}
