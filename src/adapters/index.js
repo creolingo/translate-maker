7aade970fa6ad0c2bbb51083c1883c4d479b2728
Zlatko Fedor <zfedor@gmail.com>
2016-01-06 13:04:10 +0100

    added xhr

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -1,5 +1,6 @@
 import Adapter from './Adapter';
 import File from './File';
 import Memory from './Memory';
+import XHR from './XHR';
 
-export { Adapter, File, Memory };
+export { Adapter, File, Memory, XHR };
